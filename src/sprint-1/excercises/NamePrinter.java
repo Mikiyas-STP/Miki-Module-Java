962d9b8f4bf5c8b583ec61cfd0bc2393a0884dd3
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-06-29 21:16:31 +0100

    Excerice 5

---
+++ src/sprint-1/excercises/NamePrinter.java
@@ -0,0 +1,35 @@
+//Exercise 2.4
+//Write a program that uses a switch statement to print the name of the day based on an integer
+//(1 = Monday, etc.)
+//Explain why switch in Java differs from JavaScript (e.g. type safety)
+
+public class NamePrinter {
+    public static void main(String[] args) {
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
