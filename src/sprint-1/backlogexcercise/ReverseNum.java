877e89a8707c2e7fd149e4805d02559f59c1d5dc
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-06-30 00:02:27 +0100

    reversebumber problem

---
+++ src/sprint-1/backlogexcercise/ReverseNum.java
@@ -0,0 +1,9 @@
+//Create a private method that accepts a number as an argument
+//and returns the number in reverse. For example, an input of 3956 should return 6593.
+//Create code to call this from from a main method.
+//Remember to use sensible method and variable names, and add documentation.
+//Remember to use the debugger if something goes wrong!
+//Learning Objectives
+//Create a private Java method to reverse numbers
+//Call this method from a Main method
+
