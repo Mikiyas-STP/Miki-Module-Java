9848ffb2c07c03e8eacea0874f3aca226f543803
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-06-29 21:13:45 +0100

    Excerice 1

---
+++ src/sprint-1/excercises/ClassExer.java
@@ -0,0 +1,22 @@
+//Exercise 2.1
+//Goal: Practice basic syntax and keywords.
+//
+//Create a program that:
+//
+//Declares variables of different primitive types (int, double, boolean, char).
+//Prints their values to the console.
+//Add comments explaining what each keyword does (e.g., public, static, void).
+
+public class ClassExer{
+    public static void main(String[] args){
+        int MyAge = 20;
+        double hight = 1.70;
+        boolean isWorking = false;
+        char MathGrade = 'A';
+
+        System.out.println("Age:" + MyAge);
+        System.out.println("Height is:" + hight);
+        System.out.println("Is Working:" + isWorking);
+        System.out.println("Grade:" + MathGrade);
+    }
+}
