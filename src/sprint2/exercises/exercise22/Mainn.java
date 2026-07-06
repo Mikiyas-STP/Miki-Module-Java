bcf7ce7b15992eba83cd6833200c00e0e92486c7
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-06 01:27:22 +0100

    excercise 2.2

--- src/sprint2/exercises/exercise22/Mainn.java
+++ src/sprint2/exercises/exercise22/Mainn.java
@@ -1,6 +1,5 @@
 class FileLogger {
     private boolean fileOpen = false;
-
     public void openFile() {
         if (fileOpen) {
             throw new IllegalStateException("File already open");
@@ -8,14 +7,12 @@ class FileLogger {
         System.out.println("Opening log file...");
         fileOpen = true;
     }
-
     public void writeLine(String line) {
         if (!fileOpen) {
             throw new IllegalStateException("Cannot write - file not open");
         }
         System.out.println("LOG: " + line);
     }
-
     public void closeFile() {
         if (!fileOpen) {
             throw new IllegalStateException("File already closed");
@@ -24,18 +21,13 @@ class FileLogger {
         fileOpen = false;
     }
 }
-
 class ApplicationLogger {
-
     private FileLogger logger = new FileLogger();
-
-    // Required abstraction method
     public void logInfo(String message) {
         logger.openFile();
         logger.writeLine("[INFO] " + message);
         logger.closeFile();
     }
-
     public void logError(String message) {
         logger.openFile();
         logger.writeLine("[ERROR] " + message);
@@ -43,20 +35,15 @@ class ApplicationLogger {
     }
 }
 
-public class Main {
-
+public class Mainn {
     public static void main(String[] args) {
 
-        // 1. Using FileLogger directly
         FileLogger fileLogger = new FileLogger();
-
         fileLogger.openFile();
         fileLogger.writeLine("Application started");
         fileLogger.closeFile();
 
-        // 2. Using ApplicationLogger (abstraction)
         ApplicationLogger appLogger = new ApplicationLogger();
-
         appLogger.logInfo("Application started");
         appLogger.logError("Database connection failed");
     }
