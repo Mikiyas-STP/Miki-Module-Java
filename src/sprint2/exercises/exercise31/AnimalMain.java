5287572f5cc0f261448a08248e4e810e9b23ff98
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-07 11:33:42 +0100

    solving using polymorphism - over riding in all subclasses

--- src/sprint2/exercises/exercise31/AnimalMain.java
+++ src/sprint2/exercises/exercise31/AnimalMain.java
@@ -1,67 +1,53 @@
-class Animal {
+abstract class Animal {
     private String name;
     public Animal(String name) {
         this.name = name;
     }
     public String getName() {
-
         return name;
     }
+    public abstract void perform();
 }
 class Dog extends Animal {
     public Dog(String name) {
         super(name);
     }
-    public void bark() {
+    @Override
+    public void perform() {
         System.out.println(getName() + " is barking!");
     }
 }
-
 class Cat extends Animal {
     public Cat(String name) {
         super(name);
     }
-    public void meow() {
+    @Override
+    public void perform() {
         System.out.println(getName() + " says Meow!");
     }
 }
-
 class Parrot extends Animal {
     public Parrot(String name) {
         super(name);
     }
-    public void squak() {
-        System.out.println(getName() + " is saying Squawk Squawk Squawk!");
+    @Override
+    public void perform() {
+        System.out.println(getName() + " says Squawk Squawk Squawk!");
     }
 }
-
 class AnimalShow {
     public void perform(Animal animal) {
-        if (animal instanceof Dog) {
-            Dog dog = (Dog) animal;
-            dog.bark();
-        } else if (animal instanceof Cat) {
-            Cat cat = (Cat) animal;
-            cat.meow();
-        } else if (animal instanceof Parrot) {
-            Parrot parrot = (Parrot) animal;
-            parrot.squak();
-        } else {
-            System.out.println(animal.getName() + " does not know a trick!");
-        }
+        animal.perform();
     }
 }
-
 public class AnimalMain {
     public static void main(String[] args) {
         AnimalShow show = new AnimalShow();
         Animal dog = new Dog("The Dog");
-        Animal cat = new Cat("My cat");
+        Animal cat = new Cat("My Cat");
         Animal parrot = new Parrot("Pari");
-        Animal unknown = new Animal("unknown animal");
         show.perform(dog);
         show.perform(cat);
         show.perform(parrot);
-        show.perform(unknown);
     }
 }
