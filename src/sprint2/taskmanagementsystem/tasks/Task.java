61d76d31b9dd8749d6f71a75bb464f28c60b403c
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-10 00:47:41 +0100

    class task

--- src/sprint2/taskmanagementsystem/tasks/Task.java
+++ src/sprint2/taskmanagementsystem/tasks/Task.java
@@ -3,6 +3,14 @@ package sprint2.taskmanagementsystem.tasks;
 public class Task {
     private final int id;
     private String description;
-    private boolean completion;
+    private boolean completed;
 
+    private static int nextId = 1;
+
+    public Task(String description){
+        this.id = nextId;
+        nextId++;
+        this.description = description;
+        this.completed = false;
+    }
 }
