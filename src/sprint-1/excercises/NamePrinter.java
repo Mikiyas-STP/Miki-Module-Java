5e72e0bfe95ca58503121acb30ac8aee8c2976e7
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-10 16:49:53 +0100

    Switch in Java vs JS explained

--- src/sprint-1/excercises/NamePrinter.java
+++ src/sprint-1/excercises/NamePrinter.java
@@ -2,7 +2,11 @@
 //Write a program that uses a switch statement to print the name of the day based on an integer
 //(1 = Monday, etc.)
 //Explain why switch in Java differs from JavaScript (e.g. type safety)
-
+//Answer
+//Java's switch statement is more type safer. I noticed experessions must be compatible types and label must match.
+//this is checked at compile time and avoids error during runtime in JS switch uses strict equality for comparision
+//the language is dynamically typed which means type is checked and errors are found when the program is running
+//this makes it less safer than java
 public class NamePrinter {
     public static void main(String[] args) {
         int day = 3;
