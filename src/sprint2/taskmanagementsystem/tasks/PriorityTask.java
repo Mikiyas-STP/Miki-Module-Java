abd34aa9901fa8cf45776d9e4152f4dd1d48dd4f
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-10 14:26:26 +0100

    i have defined my priority task class with the gettersand setters

--- src/sprint2/taskmanagementsystem/tasks/PriorityTask.java
+++ src/sprint2/taskmanagementsystem/tasks/PriorityTask.java
@@ -1,4 +1,20 @@
 package sprint2.taskmanagementsystem.tasks;
 
-public class PriorityTask {
+public class PriorityTask extends Task {
+    private String priority;
+
+    public PriorityTask(String description, String priority){
+        super(description);
+        this.priority = priority;
+
+    }
+    @Override
+    public String getStatus(){
+        return super.getStatus() + "Priority:" + priority;
+
+    }
+
+    public String getPriority(){
+        return priority;
+    }
 }
