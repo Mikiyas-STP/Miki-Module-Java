0d84257044a2e7c6262fefe1e4548042d3ac4d55
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-10 14:33:10 +0100

    task manager constructor

--- src/sprint2/taskmanagementsystem/management/TaskManager.java
+++ src/sprint2/taskmanagementsystem/management/TaskManager.java
@@ -1,5 +1,12 @@
 package sprint2.taskmanagementsystem.management;
 import sprint2.taskmanagementsystem.tasks.Task
 public class TaskManager {
+    private final Task[] tasks;
+    private int taskCount;
+
+    public TaskManager(){
+        tasks = new Task[10];
+        taskCount = 0;
+    }
 
 }
