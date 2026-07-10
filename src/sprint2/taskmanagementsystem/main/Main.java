e4646c7a576b7ce88e17395fc671e72a4f26fd30
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-10 15:18:59 +0100

    creating tasks

--- src/sprint2/taskmanagementsystem/main/Main.java
+++ src/sprint2/taskmanagementsystem/main/Main.java
@@ -1,4 +1,20 @@
 package sprint2.taskmanagementsystem.main;
+import sprint2.taskmanagementsystem.management.TaskManager;
+import sprint2.taskmanagementsystem.tasks.Task;
+import sprint2.taskmanagementsystem.tasks.TimedTask;
+import sprint2.taskmanagementsystem.tasks.PriorityTask;
+import java.time.LocalDate;
 
 public class Main {
+    public static void main(String[] args){
+        TaskManager manager = new TaskManager();
+        Task task1 = new Task("Do sprint 1 exercises");
+        TimedTask task2 = new TimedTask("Submit sprint2 assignment", LocalDate.now().plusDays(1));
+        PriorityTask task3 = new PriorityTask("Fix error in asssignment", "HIGH");
+
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.addTask(task3);
+
+    }
 }
