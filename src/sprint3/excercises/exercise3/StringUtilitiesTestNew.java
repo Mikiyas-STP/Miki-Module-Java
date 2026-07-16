d3d1f29dafb5b83ae5cfe344186a2ccabe5230f2
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-16 23:20:35 +0100

    test cases

--- src/sprint3/excercises/exercise3/StringUtilitiesTestNew.java
+++ src/sprint3/excercises/exercise3/StringUtilitiesTestNew.java
@@ -1,21 +1,29 @@
 package com.example;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class StringUtilitiesTestNew {
-
     private StringUtilitiesNew stringUtilities;
-
     @BeforeEach
     void setUp() {
         stringUtilities = new StringUtilitiesNew();
     }
+    @Test
+    void testIsPalindrome() {
+        assertTrue(stringUtilities.isPalindrome("level"));
+        assertTrue(stringUtilities.isPalindrome("racecar"));
 
+        assertFalse(stringUtilities.isPalindrome("hello"));
+    }
+    @Test
+    void testSplitCommaSeparatedLine() {
+        List<String> result =
+                stringUtilities.splitCommaSeparatedLine("apple,banana,orange");
 
-
+        assertEquals(
+                List.of("apple", "banana", "orange"),
+                result
+        );
+    }
 }
