47ebc8eb572070424e43fc935e518d8b5d4f9db3
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-16 23:19:42 +0100

    string utilities new

---
+++ src/sprint3/excercises/exercise3/StringUtilitiesTestNew.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilitiesTestNew {
+
+    private StringUtilitiesNew stringUtilities;
+
+    @BeforeEach
+    void setUp() {
+        stringUtilities = new StringUtilitiesNew();
+    }
+
+
+
+}
