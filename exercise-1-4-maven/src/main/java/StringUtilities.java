8f50a97a473db0f4f734fa569564a6bc6bf9b49a
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-30 03:09:47 +0100

    string utilities emthod

---
+++ exercise-1-4-maven/src/main/java/StringUtilities.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtilities {
+
+    public boolean isPalindrome(String s) {
+        String reversed = new StringBuilder(s)
+                .reverse()
+                .toString();
+
+        return s.equals(reversed);
+    }
+
+    public List<String> splitCommaSeparatedLine(String s) {
+        return Arrays.asList(s.split(","));
+    }
+}
