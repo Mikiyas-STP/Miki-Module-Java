e49c8be4c6d2717a8b98fdaaba2db11750f6f19d
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-30 04:26:18 +0100

    javadoc for the testclass

--- exercise-1-4-maven/src/test/java/CalculatorTest.java
+++ exercise-1-4-maven/src/test/java/CalculatorTest.java
@@ -2,7 +2,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This tests the behavior of the calculator class
+ */
 public class CalculatorTest {
+    /**
+     * This test case is used to check if the add method correctly add the two positive integers
+     */
 
     @Test
     void shouldAddTwoNumbers() {
