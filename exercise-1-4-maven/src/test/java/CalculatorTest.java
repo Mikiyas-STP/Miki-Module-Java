d36d66214ff76d657dc961f92e143827fe3d544b
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-30 02:59:42 +0100

    test for my calculator

--- exercise-1-4-maven/src/test/java/CalculatorTest.java
+++ exercise-1-4-maven/src/test/java/CalculatorTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+
+    @Test
+    void shouldAddTwoNumbers() {
+
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.add(2, 3);
+
+        // Assert
+        assertEquals(5, result);
+    }
 }
