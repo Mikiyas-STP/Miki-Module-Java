595b4e9a7495a607dc576321a75bb40c2f7e4524
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-30 04:38:56 +0100

    javadoc for string utilities

--- exercise-1-4-maven/src/main/java/StringUtilities.java
+++ exercise-1-4-maven/src/main/java/StringUtilities.java
@@ -1,7 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This class provide utkility methods to work with strings
+ */
 public class StringUtilities {
+    /**
+     * Check wheather a string reads the same forward and backwards
+     * @param s the string to be checked
+     * @return true if the string provided is palindrom and false if it is not.
+     */
 
     public boolean isPalindrome(String s) {
         String reversed = new StringBuilder(s)
@@ -10,6 +18,12 @@ public class StringUtilities {
 
         return s.equals(reversed);
     }
+
+    /**
+     * Splits comma separated string into a list of strings
+     * @param s is the comma separated string to be splited
+     * @return a list containing values separated by comma
+     */
 
     public List<String> splitCommaSeparatedLine(String s) {
         return Arrays.asList(s.split(","));
