613dc11984603aad41d3f8a761a74ea72a1e9afc
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-07-30 04:21:37 +0100

    javadoc for calculator-n for the class and method

--- exercise-1-4-maven/src/main/java/Calculator.java
+++ exercise-1-4-maven/src/main/java/Calculator.java
@@ -1,4 +1,13 @@
+/**
+ * This provides a basic calculator
+ */
 public class Calculator {
+    /**
+     * this method adds two integers
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
 
     public int add(int a, int b) {
         return a + b;
