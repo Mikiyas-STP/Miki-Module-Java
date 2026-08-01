b72287b2e386907c0b6e02d4d3176bfd9314ea4d
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-01 16:18:58 +0100

    delivery coordinator

---
+++ src/sprint4/exercise2/StatementDeliveryService.java
@@ -0,0 +1,13 @@
+package sprint4.exercise2;
+
+
+public class StatementDeliveryService {
+
+    public void deliverStatement(String statementContent, StatementSender statementSender){
+        statementSender.sendStatement(statementContent);
+
+    }
+    public void deliverStatement(Statement statement, StatementSender statementSender ){
+        statementSender.sendStatement(statement);
+    }
+}
