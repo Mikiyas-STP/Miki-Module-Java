0f45de3070919785163b4ecccb7a83e51287e3c2
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-01 11:56:47 +0100

    statement sender interface defined

---
+++ src/sprint4/exercise1/Main.java
@@ -0,0 +1,13 @@
+package sprint4.exercise1;
+public interface StatementSender {
+
+    /**
+     * sends the statement to the customer
+     * @param statementContent this is the text of the bank statement
+     */
+    void sendStatement(String statementContent);
+}
+
+public class Main {
+
+}
