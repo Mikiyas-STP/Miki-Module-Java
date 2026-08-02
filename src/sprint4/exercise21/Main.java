8812971cb6bfbca829a40233f7fa5a232acaf657
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 12:31:13 +0100

    inputs

--- src/sprint4/exercise21/Main.java
+++ src/sprint4/exercise21/Main.java
@@ -1,4 +1,12 @@
 package sprint4.exercise21;
 
 public class Main {
+    public static void main(String[] args){
+
+        String[] firstNames = { "Anuket", "Eryn", "Filip", "Raquel", "Eryn" };
+        String[] lastNames = { "Hassan", "Doe", "Pappas", "Clarke", "Jones" };
+        int[] grades = { 22, 45, 35, 22, 41 };
+
+
+    }
 }
