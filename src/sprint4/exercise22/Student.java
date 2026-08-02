4300e1c0fab7548aff4368678b5dc096140b0001
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 13:37:48 +0100

    overriding methods

--- src/sprint4/exercise22/Student.java
+++ src/sprint4/exercise22/Student.java
@@ -1,21 +1,37 @@
 package sprint4.exercise22;
 import java.util.Objects;
 public class Student {
-    private String[] firstName;
-    private String[] lastName;
+    private String firstName;
+    private String lastName;
 
-    public Student(String firstName, String lastName){
+    public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
 
-        public String getFirstName() {
+        public String getFirstName () {
             return firstName;
         }
-        public String getLastName() {
+        public String getLastName () {
             return lastName;
         }
-
-
+        @Override
+        public boolean equals (Object obj){
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Student other = (Student) obj;
+            return Objects.equals(firstName, other.firstName) && Objects.equals(lastName, other.lastName);
+        }
+        @Override
+        public int hashCode () {
+            return Objects.hash(firstName, lastName);
+        }
+        @Override
+        public String toString () {
+            return firstName + " " + lastName;
+        }
     }
-
 }
