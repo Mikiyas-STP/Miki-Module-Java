a70d5bebd864fb191a6eba821d6d80c9ae4f39bb
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 13:00:35 +0100

    designing my data fields and class

---
+++ src/sprint4/exercise22/Student.java
@@ -0,0 +1,21 @@
+package sprint4.exercise22;
+import java.util.Objects;
+public class Student {
+    private String[] firstName;
+    private String[] lastName;
+
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        public String getFirstName(){
+            return firstName;
+        }
+        public String getLastName(){
+            return lastName;
+        }
+
+
+    }
+
+}
