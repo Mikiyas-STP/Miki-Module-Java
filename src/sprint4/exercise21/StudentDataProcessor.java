16670fbaf11002a2adc3c4b80bcd1b8b9dcd775f
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 11:36:50 +0100

    my studentdata class : representing my data table

---
+++ src/sprint4/exercise21/StudentDataProcessor.java
@@ -0,0 +1,14 @@
+package sprint4.exercise21;
+
+public class StudentDataProcessor {
+
+    private String[] firstNames;
+    private String[] lastNames;
+    private int[] grades;
+
+    public StudentDataProcessor(String[] firstNames, String[] lastNames, int[] grades){
+        this.firstNames = firstNames;
+        this.lastNames = lastNames;
+        this.grades = grades;
+    }
+}
