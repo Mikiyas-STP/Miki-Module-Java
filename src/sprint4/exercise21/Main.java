7a3b9ed5b11937bdb594bfd4df4b9df5023d88e8
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 12:32:34 +0100

    printing using my processor instance

--- src/sprint4/exercise21/Main.java
+++ src/sprint4/exercise21/Main.java
@@ -7,6 +7,20 @@ public class Main {
         String[] lastNames = { "Hassan", "Doe", "Pappas", "Clarke", "Jones" };
         int[] grades = { 22, 45, 35, 22, 41 };
 
+        StudentDataProcessor processor = new StudentDataProcessor(firstNames, lastNames, grades);
+        System.out.println("First Names:");
+        System.out.println(processor.getFirstNames());
+        System.out.println();
 
+        System.out.println("Distinct First Names:");
+        System.out.println(processor.getDistinctFirstNames());
+        System.out.println();
+
+        System.out.println("Distinct First Names Alphabetical:");
+        System.out.println(processor.getDistinctFirstNamesInAlphabeticalOrder());
+        System.out.println();
+
+        System.out.println("Last Names -> Grades");
+        System.out.println(processor.getLastNameToGrade());
     }
 }
