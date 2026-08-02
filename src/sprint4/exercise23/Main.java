9765d9037ebece08d38f7f09aa2a12707b29850f
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 20:46:12 +0100

    initialising in main

---
+++ src/sprint4/exercise23/Main.java
@@ -0,0 +1,20 @@
+package sprint4.exercise23;
+
+import sprint4.exercise22.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+public class Main {
+
+    public static void main(String[] args) {
+        String[] firstNames = {"Anuket", "Eryn", "Filip", "Raquel", "Eryn"};
+        String[] lastNames = {"Hassan", "Doe", "Pappas", "Clarke", "Jones"};
+        int[] grades = {22, 45, 35, 22, 41};
+
+        StudentDataProcessor processor = new StudentDataProcessor( firstNames, lastNames, grades);
+
+
+    }
+
+}
