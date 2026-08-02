062599353413cc779432a4d21fccdfde12aafcee
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 13:51:29 +0100

    add Student class with equals, hashCode and toString

--- src/sprint4/exercise22/Student.java
+++ src/sprint4/exercise22/Student.java
@@ -1,37 +1,48 @@
 package sprint4.exercise22;
 import java.util.Objects;
+
 public class Student {
+
     private String firstName;
     private String lastName;
 
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
 
-        public String getFirstName () {
-            return firstName;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
         }
-        public String getLastName () {
-            return lastName;
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
         }
-        @Override
-        public boolean equals (Object obj){
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-            Student other = (Student) obj;
-            return Objects.equals(firstName, other.firstName) && Objects.equals(lastName, other.lastName);
-        }
-        @Override
-        public int hashCode () {
-            return Objects.hash(firstName, lastName);
-        }
-        @Override
-        public String toString () {
-            return firstName + " " + lastName;
-        }
+
+        Student other = (Student) obj;
+
+        return Objects.equals(firstName, other.firstName)
+                && Objects.equals(lastName, other.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
     }
 }
