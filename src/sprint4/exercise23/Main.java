8c6b9494af5de1a75a72bbf7b4fdba9f4b1e34c7
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2026-08-02 21:30:56 +0100

    operating on my data

--- src/sprint4/exercise23/Main.java
+++ src/sprint4/exercise23/Main.java
@@ -20,5 +20,28 @@ public class Main {
         Set<String> sortedNames = processor.getDistinctFirstNamesInAlphabeticalOrder();
         Map<Student, Integer> students = processor.getStudentToGrade();
 
+        //creating
+        names.add("Miki");
+        uniqueNames.add("Miki");
+        sortedNames.add("Miki");
+        Student mike = new Student("Miki", "Smith");
+        students.put(mike, 50);
+
+        //deleting
+        names.remove("Filip");
+        uniqueNames.remove("Filip");
+        sortedNames.remove("Filip");
+        Student filip = new Student("Filip", "Pappas");
+        students.remove(filip);
+
+        //update
+        Student eryn = new Student("Eryn", "Doe");
+        students.put(eryn, 0);
+
+        System.out.println(names);
+        System.out.println(uniqueNames);
+        System.out.println(sortedNames);
+        System.out.println(students);
+
     }
 }
